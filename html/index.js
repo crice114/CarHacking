33627693e15c1018a556fb48ad90b26fca2b35bf
Cody Rice <79558669+crice114@users.noreply.github.com>
2022-07-25 21:10:19 -0700

    Update index.js

--- html/index.js
+++ html/index.js
@@ -6,8 +6,13 @@ function onDomReadyHandler(event) {
     socket.on('carMessage', (data) => {
         var revs = document.getElementsByTagName('canvas')[0];
         var speedo = document.getElementsByTagName('canvas')[1];
+        
+        var fuel = document.getElementsByTagName('canvas')[2];
+        
         speedo.setAttribute('data-value', data.speed)
         revs.setAttribute('data-value', data.revs)
+        
+        fuel.setAttribute('data-value', data.fuel)
         console.log(data)
     })
 }
